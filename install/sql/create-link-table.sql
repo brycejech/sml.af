f6d0ff57ec048b588c2c795141de1ec2f7a5ebfd
Bryce Jech <bryce@brycejech.com>
2018-05-22 21:33:34 -0500

    update table definition

--- install/sql/create-link-table.sql
+++ install/sql/create-link-table.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS link CASCADE;
 
 CREATE TABLE link
 (
-    id       BIGINT     PRIMARY KEY,
-    link     TEXT       UNIQUE NOT NULL,
-    url      TEXT       NOT NULL,
-    created  TIMESTAMP  DEFAULT now()::timestamp
+    id         BIGINT     PRIMARY KEY,
+    url        TEXT       NOT NULL,
+    short_url  TEXT       NOT NULL,
+    created    TIMESTAMP  DEFAULT now()::timestamp
 );
