f0db9178d78bcd3692cc9ca6514a7ae59f0a44dd
Bryce Jech <bryce@brycejech.com>
2018-05-23 22:19:17 -0500

    register logRequest query

--- sql/queries.js
+++ sql/queries.js
@@ -4,12 +4,14 @@ const fs = require('fs');
 
 const dir = __dirname
 
-const getLinkByUrl    = fs.readFileSync(dir + '/queries/get-link-by-url.sql', 'utf8'),
-      getLinkByHash   = fs.readFileSync(dir + '/queries/get-link-by-hash.sql', 'utf8'),
-      linkExists      = fs.readFileSync(dir + '/queries/link-exists.sql', 'utf8'),
+const getLinkByUrl    = fs.readFileSync(dir + '/queries/get-link-by-url.sql',     'utf8'),
+      getLinkByHash   = fs.readFileSync(dir + '/queries/get-link-by-hash.sql',    'utf8'),
+      linkExists      = fs.readFileSync(dir + '/queries/link-exists.sql',         'utf8'),
       getLinkOrNextID = fs.readFileSync(dir + '/queries/get-link-or-next-id.sql', 'utf8'),
-      addLink         = fs.readFileSync(dir + '/queries/add-link.sql', 'utf8'),
-      getAllLinks     = fs.readFileSync(dir + '/queries/get-all-links.sql', 'utf8');
+      addLink         = fs.readFileSync(dir + '/queries/add-link.sql',            'utf8'),
+      getAllLinks     = fs.readFileSync(dir + '/queries/get-all-links.sql',       'utf8'),
+
+      logRequest      = fs.readFileSync(dir + '/queries/log-request.sql',         'utf8');
 
 
 const queries = {
@@ -18,7 +20,9 @@ const queries = {
     linkExists,
     getLinkOrNextID,
     addLink,
-    getAllLinks
+    getAllLinks,
+
+    logRequest
 }
 
 module.exports = queries;
